1ad3ffbef13af24f6db5bd6ca391e0ab0ecaaa5f
Caue Garcia <cauerg7@outlook.com>
2024-02-05 14:58:23 -0300

    insert e get paged cities

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<CityDTO> findAllPaged(Pageable pageable) {
+		Page<City> list = repository.findAll(pageable);
+		return list.map(x -> new CityDTO(x));
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+}
